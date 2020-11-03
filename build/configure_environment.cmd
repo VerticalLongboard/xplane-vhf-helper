9199c3f31ddfad18c4854dcc0d977923631ed3b5
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2020-11-03 12:13:56 -0500

    Moved test dependencies to separate folder.

--- build/configure_environment.cmd
+++ build/configure_environment.cmd
@@ -1,3 +1,4 @@
+@REM DO NOT change this file to setup your local environment. Instead, run any task at least once and look up LOCAL_ENVIRONMENT_CONFIGURATION.cmd
 if not exist LOCAL_ENVIRONMENT_CONFIGURATION.cmd (
     (
         echo set NSIS_EXECUTABLE="C:\Program Files (x86)\NSIS\makensis.exe"
@@ -5,10 +6,11 @@ if not exist LOCAL_ENVIRONMENT_CONFIGURATION.cmd (
         echo set LUA_DEFAULT_MODULES_PATH="C:\Program Files (x86)\Lua\5.1\lua"
         echo set SEVEN_ZIP_EXECUTABLE="C:\Program Files\7-Zip\7z.exe"
         echo set PACKETSENDER_EXECUTABLE="C:\Program Files\PacketSender\packetsender.exe"
-        echo set XPLANE_PATH=C:\X-Plane 11
+        echo set XPLANE_PATH="C:\X-Plane 11"
     ) > LOCAL_ENVIRONMENT_CONFIGURATION.cmd
 )
 
 call LOCAL_ENVIRONMENT_CONFIGURATION.cmd
 
 set LUA_DEFAULT_MODULES_PATH=%LUA_DEFAULT_MODULES_PATH:"=%
+set XPLANE_PATH=%XPLANE_PATH:"=%
